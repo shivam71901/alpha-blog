6865e4951cb91627f9086ae4cd1f0fe527055118
shivam71901 <shivamsn363@gmail.com>
2019-10-23 11:13:37 +0530

    style form partial

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,13 @@
+
+//= require jquery
+//= require bootstrap-sprockets
+
+
+
+
+
+
+
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
@@ -16,12 +26,3 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
-
-
-//= require jquery_ujs
-//= require jquery
-//= require bootstrap-sprockets
-//= require bootstrap
-//= require turbolinks
-//= require_tree
