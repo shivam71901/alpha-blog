cf930e9bb303f9f817db4d30ea17c7ed969938b3
shivam71901 <shivamsn363@gmail.com>
2019-10-28 19:12:13 +0530

    signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   root 'welcome#home'
   get 'about', to: 'welcome#about'
   resources :articles
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
